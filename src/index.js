0a089ad1692396838f4ae8ba001aebe48f9138a9
mannycolon <colonmanuel7@gmail.com>
2018-11-21 17:17:04 -0500

    restructure the way the components are exported and will be imported. now components dont share the same import/export

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
-import React from 'react'
-import BraintreeHostedFields from './components/BraintreeHostedFields'
-import BraintreeDropIn from './components/BraintreeDropIn'
-
-const BraintreeWebDropIn = (props) => {
-  const { isHostedFields } = props
-
-  if (isHostedFields) {
-    return <BraintreeHostedFields {...props}/>
-  } else {
-    return <BraintreeDropIn {...props}/>
-  }
-}
-
-export default BraintreeWebDropIn
+export { default as BraintreeHostedFields } from './components/BraintreeHostedFields'
+export { default as BraintreeDropIn } from './components/BraintreeDropIn'
