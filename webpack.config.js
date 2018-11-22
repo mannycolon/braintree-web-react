79848dbfa96cef0029adccf8521f3c124c6a1e67
mannycolon <colonmanuel7@gmail.com>
2018-11-21 19:43:46 -0500

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
+var path = require('path');
+
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    libraryTarget: 'commonjs2'
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-          options: {
-            babelrc: true
-          }
+          loader: "babel-loader"
         }
       }
     ]
