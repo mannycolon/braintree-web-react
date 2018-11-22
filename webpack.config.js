d29ba273ea01e9facb31289909dc15f2a6d48464
mannycolon <colonmanuel7@gmail.com>
2018-11-21 19:40:47 -0500

    tests

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"]
+            babelrc: true
           }
         }
       }
