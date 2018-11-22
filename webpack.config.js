22ca9102b46b94e95f8f887a0099bd4eae5fab0c
mannycolon <colonmanuel7@gmail.com>
2018-11-21 20:12:18 -0500

    Added actual fix for import/export of compoennts

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  // entry: './src/index.js',
-  // output: {
-  //   path: path.resolve(__dirname, 'dist'),
-  //   filename: 'main.js',
-  //   libraryTarget: 'commonjs2'
-  // },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    libraryTarget: 'commonjs2'
+  },
   module: {
     rules: [
       {
